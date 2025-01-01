ce30bb63824f46b5ad200d2c8e1b80c4f1e83aab
Meir Roth <12494197+meirroth@users.noreply.github.com>
2025-01-01 13:50:22 +0200

    use infinite scroll

--- components/PopularMovies.vue
+++ components/PopularMovies.vue
@@ -4,16 +4,32 @@
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5"
     >
       <MovieCard v-for="movie in movies" :key="movie.id" :movie />
+      <div v-if="isLoading" class="text-center col-span-full">Loading...</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useInfiniteScroll } from "@vueuse/core";
+
 const store = useMoviesStore();
+const scrollContainer = ref<HTMLElement>();
 
 const movies = computed(() => store.movies);
+const isLoading = computed(() => store.isLoading);
+const canLoadMore = computed(() => store.canLoadMore);
 
 await callOnce(async () => {
   await store.fetchPopularMovies();
 });
+
+useInfiniteScroll(
+  scrollContainer,
+  async () => {
+    if (canLoadMore.value) {
+      await store.loadMoreMovies();
+    }
+  },
+  { distance: 10 }
+);
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+
   devtools: { enabled: true },
+
   modules: ["@nuxt/ui", "@pinia/nuxt"],
+
+  runtimeConfig: {
+    public: {
+      tmdbApiUrl: process.env.TMDB_API_URL,
+      tmdbApiKey: process.env.TMDB_API_KEY,
+    },
+  },
 });
