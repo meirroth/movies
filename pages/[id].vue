85b699dd6c05df2957c1a1c8180f395ac52b0600
Meir Roth <12494197+meirroth@users.noreply.github.com>
2025-01-01 14:13:08 +0200

    seo meta

--- pages/[id].vue
+++ pages/[id].vue
@@ -11,4 +11,9 @@ const movie = computed(() => store.movie);
 await callOnce(async () => {
   await store.loadMovieDetails(Number(id));
 });
+
+useSeoMeta({
+  title: movie.value?.title,
+  description: movie.value?.overview,
+});
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template>
   <PopularMovies />
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "Popular Movies",
+  description: "Browse the most popular movies in the world.",
+});
+</script>
